d9fcb2b7eb44298e02e608b1b17f985a7575d76d
viniciussaturnino <viniciussaturnino07@hotmail.com>
2020-08-24 15:09:56 -0300

    Solved error with migrations

--- src/database/migrations/20200824111008_create_advisor.js
+++ src/database/migrations/20200824111008_create_advisor.js
@@ -1,11 +1,13 @@
-export function up(knex) {
+// eslint-disable-next-line func-names
+exports.up = function (knex) {
   // eslint-disable-next-line func-names
   return knex.schema.createTable('advisors', function (table) {
-    table.increments();
+    table.increments('id').primary();
     table.string('name').notNullable();
   });
-}
+};
 
-export function down(knex) {
+// eslint-disable-next-line func-names
+exports.down = function (knex) {
   return knex.schema.dropTable('advisors');
-}
+};
--- src/database/migrations/20200824143450_create_client.js
+++ src/database/migrations/20200824143450_create_client.js
@@ -1,17 +1,18 @@
-export function up(knex) {
+// eslint-disable-next-line func-names
+exports.up = function (knex) {
   // eslint-disable-next-line func-names
   return knex.schema.createTable('clients', function (table) {
-    table.increments();
+    table.increments('id').primary();
     table.string('name').notNullable();
     table.string('cpf', 11).notNullable();
     table.string('email').notNullable();
 
-    table.integer('advisor_id').notNullable();
-
+    table.integer('advisor_id').unsigned();
     table.foreign('advisor_id').references('id').inTable('advisors');
   });
-}
+};
 
-export function down(knex) {
+// eslint-disable-next-line func-names
+exports.down = function (knex) {
   return knex.schema.dropTable('clients');
-}
+};
